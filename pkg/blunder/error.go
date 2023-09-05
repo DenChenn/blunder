506c89a3c62e984cda3c306e58a2c5bf2d3e133f
DenChenn <aaaa102234@gmail.com>
2023-09-05 17:37:42 +0800

    feat: generate native compatible error

--- pkg/blunder/error.go
+++ pkg/blunder/error.go
@@ -1,40 +1,25 @@
 package blunder
 
-type Error interface {
-	Error() string
-	GetId() string
-	GetHttpStatusCode() int
-	GetGrpcStatusCode() int
-	GetErrorCode() string
-	GetMessage() string
-	WithCustomMessage(msg string) Error
-}
-
 type Unexpected struct {
-	Id             string `json:"id"`
 	HttpStatusCode int    `json:"http_status_code"`
 	GrpcStatusCode int    `json:"grpc_status_code"`
 	ErrorCode      string `json:"error_code"`
 	Message        string `json:"message"`
+	Err            error  `json:"err"`
 }
 
 var (
 	ErrUndefined = &Unexpected{
-		Id:             "undefined_error",
 		HttpStatusCode: 500,
 		GrpcStatusCode: 13,
 		ErrorCode:      "UNDEFINED_ERROR",
 		Message:        "Unexpected error happened.",
 	}
-	_ Error = ErrUndefined
+	_ error = ErrUndefined
 )
 
 func (e *Unexpected) Error() string {
 	return e.Message
-}
-
-func (e *Unexpected) GetId() string {
-	return e.Id
 }
 
 func (e *Unexpected) GetHttpStatusCode() int {
@@ -53,8 +38,10 @@ func (e *Unexpected) GetMessage() string {
 	return e.Message
 }
 
-func (e *Unexpected) WithCustomMessage(msg string) Error {
-	newE := *e
-	newE.Message = msg
-	return &newE
+func (e *Unexpected) Wrap(err error) {
+	e.Err = err
+}
+
+func (e *Unexpected) Unwrap() error {
+	return e.Err
 }
